6223a681e64435b85970a1e5c9f260a500d3caf4
Jake Chestnut <jacobschestnut@gmail.com>
2025-03-04 18:00:57 -0600

    format user cards so they do not overlap

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,44 +1,41 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { UserDB } from '@/types/UserDB';
-import UserCard from '@/components/UserCard';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { UserDB } from "@/types/UserDB";
+import UserCard from "@/components/UserCard";
 
 const UsersList = () => {
   const router = useRouter();
   const [users, setUsers] = useState<UserDB[]>([]);
-  
+
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const res = await fetch('/api/users');
+        const res = await fetch("/api/users");
         if (!res.ok) {
-          throw new Error('Failed to fetch users in client');
-        }  
+          throw new Error("Failed to fetch users in client");
+        }
         const data = await res.json();
         setUsers(data);
       } catch (error) {
         console.log(error);
       }
     };
-    
+
     fetchUsers();
   }, []);
-  
+
   return (
-    <div className='flex flex-col h-screen bg-gradient-to-r from-blue-500 via-teal-500 to-green-500 max-w-screen mx-auto p-6 justify-start items-center'>
-      <div className="flex justify-center items-center">
+    <div className="flex flex-col h-full bg-gradient-to-r from-blue-500 via-teal-500 to-green-500 max-w-screen mx-auto p-6 justify-start items-center">
+      <div className="flex flex-wrap justify-center items-center">
         {users.map((user) => (
-          <UserCard
-            key={user.id}
-            user={user}
-          />
+          <UserCard key={user.id} user={user} />
         ))}
       </div>
       <button
         className="btn btn-outline m-10 py-10 w-100"
-        onClick={() => router.push('/form')}
+        onClick={() => router.push("/form")}
       >
         <h3 className="text-xl text-white">New User</h3>
       </button>
